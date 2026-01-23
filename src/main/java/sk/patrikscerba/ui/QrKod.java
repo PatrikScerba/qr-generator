a7a26d8ef095036004aa8ddb88ade06c7dc2f806
Patrik Scerba <patrikscerba@gmail.com>
2026-01-23 10:53:38 +0100

    feat(ui): formulár pre zadanie údajov a náhľad QR

--- src/main/java/sk/patrikscerba/ui/QrKod.java
+++ src/main/java/sk/patrikscerba/ui/QrKod.java
@@ -5,6 +5,21 @@ import javax.swing.*;
 public class QrKod extends JFrame {
 
     private JPanel mainPanel;
+    private JTextField jTextKrstneMeno;
+    private JTextField jTextPriezvisko;
+    private JTextField jTextTelefonneCislo;
+    private JTextField jTextEmail;
+
+    private JLabel krstneMenoLabel;
+    private JLabel priezviskoLabel;
+    private JLabel telefonneCisloLabel;
+    private JLabel emailLabel;
+
+    private JLabel qrObrazokLabel;
+
+    private JButton generovatQrButton;
+    private JButton ulozitQrButton;
+    private JButton znovaVygenerovatButton;
 
 
     public QrKod() {
@@ -13,5 +28,7 @@ public class QrKod extends JFrame {
         setSize(500, 650);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-}
+
+
+    }
 }
